466f2dc63ff9f1e1b972038d3d65b1bbb60d5618
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-02-11 10:05:52 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        // create and object person
         Person person = new Person();
 
-        person.setFirstName("");//First name is set to empty srring
+        // set the properties of the created object
+        person.setFirstName("");//First name is set to empty string
         person.setLastName("");//Last name is set to empty string
         person.setAge(10);
 
+        //print the full name of the person
         System.out.println("Full name = " + person.getFullName());
         System.out.println("teen = " + person.isTeen());
 
+        // set first name 
         person.setFirstName("John");
         person.setAge(18);
 
